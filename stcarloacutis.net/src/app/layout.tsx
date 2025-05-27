476d0d9f5847cabbbd43bec3f752bded23fa768c
Noah Bakayou <NoahBakayou@gmail.com>
2025-05-27 16:28:02 -0700

    updated favicon

--- stcarloacutis.net/src/app/layout.tsx
+++ stcarloacutis.net/src/app/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/eucharist.png" type="image/png" />
+        <link rel="icon" href="/favicon_io/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
         <meta property="og:image" content="/eucharist.png" />
         <meta name="twitter:image" content="/eucharist.png" />
       </head>
